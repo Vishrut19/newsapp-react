4f795ba2de13b9cda9b2c34fa58f898172c64d94
Vishrut19 <vishrutagarwalla19@gmail.com>
2022-11-20 10:22:36 +0530

    Updates

---
+++ precache-manifest.d859946fbd8dbcbfe0ebb4bd719ea5af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1153c384d2ace39bec8",
+    "url": "/newsapp-react/static/css/main.88f208ad.chunk.css"
+  },
+  {
+    "revision": "c1153c384d2ace39bec8",
+    "url": "/newsapp-react/static/js/main.c1153c38.chunk.js"
+  },
+  {
+    "revision": "fbee9149cc341e4ff9a0",
+    "url": "/newsapp-react/static/js/1.fbee9149.chunk.js"
+  },
+  {
+    "revision": "d88902d9366a620932ba",
+    "url": "/newsapp-react/static/js/2.d88902d9.chunk.js"
+  },
+  {
+    "revision": "5db7aa0153f9d8b605f3",
+    "url": "/newsapp-react/static/js/runtime~main.5db7aa01.js"
+  },
+  {
+    "revision": "bb9408965447abd2d39004b36d7cc3d3",
+    "url": "/newsapp-react/index.html"
+  }
+];
